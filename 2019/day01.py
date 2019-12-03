e1c36e94fd3fc102b0a2de7d5f7b66b0ae942daf
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2019-12-03 09:48:26 +0100

    move files to correct location

--- 2019/day01.py
+++ 2019/day01.py
@@ -1,6 +1,7 @@
 from math import floor
 from typing import Callable
 
+
 def calculate_fuel(mass: int) -> int:
     """
     To find the fuel required for a module, take its mass,
@@ -8,11 +9,12 @@ def calculate_fuel(mass: int) -> int:
     """
     return floor(mass / 3) - 2
 
+
 def calculate_fuel_better(mass: int) -> int:
     """
     For each module mass, calculate its fuel and add it to
-    the total. Then, treat the fuel amount you just calculated 
-    as the input mass and repeat the process, continuing until 
+    the total. Then, treat the fuel amount you just calculated
+    as the input mass and repeat the process, continuing until
     a fuel requirement is zero or negative.
     """
     fuel = calculate_fuel(mass)
@@ -22,17 +24,19 @@ def calculate_fuel_better(mass: int) -> int:
         fuel = calculate_fuel(fuel)
     return total_fuel
 
+
 def main(func: Callable) -> int:
     """
-    What is the sum of the fuel requirements for all of 
+    What is the sum of the fuel requirements for all of
     the modules on your spacecraft?
     """
-    with open('data/day01.txt') as f:
+    with open("data/day01.txt") as f:
         masses = map(int, f.read().splitlines())
 
     return sum(func(mass) for mass in masses)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     assert calculate_fuel(12) == 2
     assert calculate_fuel(14) == 2
     assert calculate_fuel(1969) == 654
@@ -45,6 +49,3 @@ if __name__ == '__main__':
     assert calculate_fuel_better(100756) == 50346
 
     print(main(calculate_fuel_better))
-
-
-
