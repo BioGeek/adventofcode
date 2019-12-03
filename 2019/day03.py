86dc990f5e8708418e6930b5d222d2352e7969c6
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2019-12-03 13:06:20 +0100

    Crosses where wire crosses itself do not countgit add day03.py

--- 2019/day03.py
+++ 2019/day03.py
@@ -1,14 +1,17 @@
 from collections import defaultdict
 from typing import DefaultDict, Tuple, List, Union
+from pprint import pprint
 
 Coordinates = DefaultDict[Tuple[int, int], int]
 
-def manhattan_distance(x: int, y: int) -> int:
-    return abs(x) + abs(y)
+def manhattan_distance(x: int, y: int, origin_x: int = 0, origin_y: int = 0) -> int:
+    """Returns the sum of the absolute difference of cartesian coordinates"""
+    return abs(x - origin_x) + abs(y - origin_y)
 
 def create_coords(wires: List[str]) -> Coordinates:
-    coordinates = defaultdict(int) # type: Coordinates
-    for wire in wires:
+    """For each coordinate that is passes, increase the count by 1."""
+    coordinates = defaultdict(list) # type: Coordinates
+    for i, wire in enumerate(wires):
         x, y = 0, 0
         for instruction in wire.split(','):
             direction, distance = instruction[0], int(instruction[1:])
@@ -23,22 +26,46 @@ def create_coords(wires: List[str]) -> Coordinates:
                     y -= 1
                 else:
                     raise RuntimeError(f"Unrecognized direction: {direction}")
-                coordinates[(x, y)] += 1
+                coordinates[(x, y)].append(i)
     return coordinates
 
 def get_data() -> List[str]:
+    """Read in the data."""
     with open('data/day03.txt') as f:
         wires = f.read().splitlines()
     return wires
 
 def find_crossing_wires(coordinates: Coordinates) -> int :
+    """Return the shortest distance where hte wires cross."""
     shortest_distance = 1_000_000
     for coords in coordinates:
-        if coordinates[coords] > 1 and coords != (0, 0):
-            # print(coords)
+        if sorted(coordinates[coords]) == [0, 1] and coords != (0, 0):
             if manhattan_distance(*coords) < shortest_distance:
                 shortest_distance = manhattan_distance(*coords)
     return shortest_distance
+
+def draw(coordinates):
+    crossing_coordinates = {k: manhattan_distance(*k) for (k, v) in coordinates.items() if sorted(v) == [0, 1]}
+    pprint(crossing_coordinates)
+    xs = [c[0] for c in coordinates.keys()]
+    ys = [c[1] for c in coordinates.keys()]
+    min_x = min(min(xs), 0)
+    min_y = min(min(ys), 0)
+    max_x = max(max(xs), 0)
+    max_y = max(max(ys), 0)
+    
+    for y in range(max_y+1, min_y-2, -1):
+        for x in range(min_x-1, max_x+2):
+            if (x, y) == (0, 0):
+                print('o', end = '')
+            elif (x,y) in coordinates:
+                if sorted(coordinates[(x, y)]) == [1, 2]:
+                    print('X', end = '')
+                else:
+                    print('+', end = '')
+            else:
+                print('.', end= '')
+        print() 
 
 def main() -> int:
     wires = get_data()
@@ -47,10 +74,9 @@ def main() -> int:
 
 
 if __name__ == '__main__':
-    # print(crossing_wires())
-
     wires = ["R8,U5,L5,D3", "U7,R6,D4,L4"]
     assert find_crossing_wires(create_coords(wires)) == 6
+    # draw(create_coords(wires))
 
     wires = ["R75,D30,R83,U83,L12,D49,R71,U7,L72",
              "U62,R66,U55,R34,D71,R55,D58,R83"]
@@ -61,5 +87,3 @@ if __name__ == '__main__':
     assert find_crossing_wires(create_coords(wires)) == 135
 
     print(main())
-    
-
