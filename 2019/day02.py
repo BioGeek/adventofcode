97977a600eab9e87bfc8468bcddee4e9c91a5594
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2019-12-03 16:23:57 +0100

    reformat with black

--- 2019/day02.py
+++ 2019/day02.py
@@ -5,7 +5,7 @@ def process(code: List[int]) -> List[int]:
     """Process Intcode."""
     for i in range(0, len(code), 4):
         try:
-            opcode, input_1, input_2, output = code[i:i+4]
+            opcode, input_1, input_2, output = code[i : i + 4]
         except ValueError:
             opcode = code[i]
         if opcode == 1:
@@ -62,10 +62,21 @@ if __name__ == "__main__":
     assert process([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
     assert process([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
     assert process([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
-    assert process([1, 1, 1, 4, 99, 5, 6, 0, 99]) == \
-                   [30, 1, 1, 4, 2, 5, 6, 0, 99]
-    assert process([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]) == \
-                   [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+    assert process([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    assert process([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]) == [
+        3500,
+        9,
+        10,
+        70,
+        2,
+        3,
+        11,
+        0,
+        99,
+        30,
+        40,
+        50,
+    ]
 
     print(main())
     print(main2())
--- 2019/day03.py
+++ 2019/day03.py
@@ -6,43 +6,48 @@ from functools import reduce
 
 Coordinates = Dict[Tuple[int, int], int]
 
+
 def manhattan_distance(x: int, y: int, origin_x: int = 0, origin_y: int = 0) -> int:
     """Returns the sum of the absolute difference of cartesian coordinates"""
     return abs(x - origin_x) + abs(y - origin_y)
 
+
 def create_coords(wire: str) -> Coordinates:
-    """For each coordinate that is passes, increase the count by 1."""
-    coordinates = {} # type: Coordinates
+    """For each coordinate that is passed, increase the length by 1."""
+    coordinates = {}  # type: Coordinates
     x, y = 0, 0
     length = 0
-    for instruction in wire.split(','):
+    for instruction in wire.split(","):
         direction, distance = instruction[0], int(instruction[1:])
         for step in range(distance):
-            if direction == 'R':
+            if direction == "R":
                 x += 1
-            elif direction == 'L':
+            elif direction == "L":
                 x -= 1
-            elif direction == 'U':
+            elif direction == "U":
                 y += 1
-            elif direction == 'D':
+            elif direction == "D":
                 y -= 1
             else:
                 raise RuntimeError(f"Unrecognized direction: {direction}")
             length += 1
-            coordinates[(x, y)] = length 
+            coordinates[(x, y)] = length
     return coordinates
+
 
 def get_data() -> List[str]:
     """Read in the data."""
-    with open('data/day03.txt') as f:
+    with open("data/day03.txt") as f:
         wires = f.read().splitlines()
     return wires
 
-def find_crossing_wires(wires: List[str] ) -> int :
+
+def find_crossing_wires(wires: List[str]) -> int:
     """Return the shortest distance where the wires cross."""
     coords_1, coords_2 = [set(create_coords(wire).keys()) for wire in wires]
     common_coordinates = coords_1.intersection(coords_2)
     return min(manhattan_distance(*coords) for coords in common_coordinates)
+
 
 def find_minimal_delay(wires: List[str]) -> int:
     """Calculate the number of steps each wire takes to reach each intersection; 
@@ -63,19 +68,20 @@ def draw(wires):
     min_y = min(min(ys), 0)
     max_x = max(max(xs), 0)
     max_y = max(max(ys), 0)
-    
-    for y in range(max_y+1, min_y-2, -1):
-        for x in range(min_x-1, max_x+2):
+
+    for y in range(max_y + 1, min_y - 2, -1):
+        for x in range(min_x - 1, max_x + 2):
             if (x, y) == (0, 0):
-                print('o', end = '')
-            elif (x,y) in all_coordinates:
+                print("o", end="")
+            elif (x, y) in all_coordinates:
                 if (x, y) in common_coordinates:
-                    print('X', end = '')
+                    print("X", end="")
                 else:
-                    print('+', end = '')
+                    print("+", end="")
             else:
-                print('.', end= '')
-        print() 
+                print(".", end="")
+        print()
+
 
 def main(part=1) -> int:
     wires = get_data()
@@ -84,12 +90,14 @@ def main(part=1) -> int:
     else:
         return find_minimal_delay(wires)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     wires_1 = ["R8,U5,L5,D3", "U7,R6,D4,L4"]
-    wires_2 = ["R75,D30,R83,U83,L12,D49,R71,U7,L72",
-               "U62,R66,U55,R34,D71,R55,D58,R83"]
-    wires_3 = ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-               "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+    wires_2 = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
+    wires_3 = [
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+    ]
 
     assert find_crossing_wires(wires_1) == 6
     assert find_crossing_wires(wires_2) == 159
