8feeb2a154f635f3b1979a835bba552019e21ee0
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2019-12-04 06:53:58 +0100

    day 4 part 2

--- 2019/day04.py
+++ 2019/day04.py
@@ -1,6 +1,12 @@
-def password(i: int) -> bool:
+from collections import Counter
+
+def password(i: int, part: int = 1) -> bool:
     n = str(i)
-    return len(n) == 6 and two_adjecent_digits(n) and never_decreases(n)
+    result = len(n) == 6 and two_adjecent_digits(n) and never_decreases(n)
+    if part == 1:
+        return result
+    else:
+        return result and not_part_of_larger_group(n)
 
 def two_adjecent_digits(n: str) -> bool:
     return any(i == j for i, j in zip(n, n[1:]))
@@ -8,8 +14,17 @@ def two_adjecent_digits(n: str) -> bool:
 def never_decreases(n: str) -> bool:
     return all(i <= j for i, j in zip(n, n[1:]))
 
-def main(low: int, high: int) -> int:
-    return sum(password(n) for n in range(low, high))
+def not_part_of_larger_group(n: str) -> bool:
+    c = Counter(n)
+    if 2 in c.values():
+        return True
+    elif any(v > 2 for v in c.values()):
+        return False
+    else:
+        return False
+    
+def main(low: int, high: int, part: int=1) -> int:
+    return sum(password(n, part) for n in range(low, high))
 
 if __name__ == '__main__':
     assert never_decreases("111123")
@@ -22,3 +37,10 @@ if __name__ == '__main__':
     low = 265275
     high = 781584
     print(main(low, high))
+
+    assert not_part_of_larger_group("112233")
+    assert not not_part_of_larger_group("123444")
+    assert not_part_of_larger_group("111122")
+
+    print(main(low, high, part=2))
+
