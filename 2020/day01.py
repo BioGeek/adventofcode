33c54091d60713802f97b45ca5f2274cab4de71e
Jeroen Van Goey <jeroen.vangoey@mail.com>
2020-12-01 10:40:54 +0100

    mypy and black code style fixes

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,30 +1,34 @@
-# part1:  find the two entries that sum to 2020 and then multiply those two numbers together.
+# part1: find the two entries that sum to 2020 and then multiply those two numbers together.
 # part2: find three numbers in your expense report that meet the same criteria.
-from typing import List
 from itertools import combinations
 from math import prod
+from typing import Iterator
 
-def sum_to_2020(expenses: List[int], part: int=1) -> int:
-    for combination in combinations(expenses, part+1):
+
+def sum_to_2020(expenses: Iterator[int], part: int = 1) -> int:
+    for combination in combinations(expenses, part + 1):
         if sum(combination) == 2020:
             return prod(combination)
+    assert False  # To make mypy happy
 
 
-def expenses(report: str) -> List[int]:
+def expenses(report: str) -> Iterator[int]:
     return map(int, report.splitlines())
 
-def main(part: int=1) -> int:
+
+def main(part: int = 1) -> int:
     with open("data/day01.txt") as f:
         report = f.read()
     return sum_to_2020(expenses(report), part)
 
-if __name__ == '__main__':
-    report = '''1721
+
+if __name__ == "__main__":
+    report = """1721
 979
 366
 299
 675
-1456'''
+1456"""
     assert sum_to_2020(expenses(report)) == 514579
     assert sum_to_2020(expenses(report), part=2) == 241861950
 
