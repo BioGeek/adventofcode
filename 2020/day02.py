ce573e02b1ed87595127a03cef779eb8ddddf52c
Jeroen Van Goey <jeroen.vangoey@mail.com>
2020-12-02 14:25:03 +0100

    describe password policies

--- 2020/day02.py
+++ 2020/day02.py
@@ -13,8 +13,14 @@ class Policy:
 def is_valid_password(policy: Policy, password: str, part=1) -> bool:
     """Returns if a password is valid according to the policy."""
     if part == 1:
+        # The password policy indicates the lowest and highest number of times a given 
+        # letter must appear for the password to be valid.
         return policy.lower <= password.count(policy.char) <= policy.upper
     else:
+        # The policy describes two positions in the password, where 1 means the first 
+        # character, 2 means the second character, and so on. Exactly one of these 
+        # positions must contain the given letter. Other occurrences of the letter are 
+        # irrelevant for the purposes of policy enforcement.
         return (
             sum(
                 [
@@ -41,7 +47,9 @@ def parse(data: str) -> Iterator[Tuple[Policy, str]]:
 def main(part: int = 1):
     with open("data/day02.txt") as fh:
         data = fh.read()
-    return sum(is_valid_password(policy, password, part) for policy, password in parse(data))
+    return sum(
+        is_valid_password(policy, password, part) for policy, password in parse(data)
+    )
 
 
 if __name__ == "__main__":
