37fe5030ac31cba05720053624dda0fbc7bb12b1
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2021-12-01 17:08:56 +0100

    2021, day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,10 +1,10 @@
-
-from typing import Iterator
 from itertools import pairwise, tee
+from typing import Iterator
+
 
 def triplewise(iterable):
     """triplewise('ABCDEFG') --> ABC BCD CDE DEF EFG"""
-    a, b,  c= tee(iterable, 3)
+    a, b, c = tee(iterable, 3)
     next(b, None)
     next(c, None)
     next(c, None)
@@ -14,7 +14,8 @@ def triplewise(iterable):
 def sonar_sweep(sea_floor_depths: Iterator[int], part: int = 1) -> int:
     if part == 1:
         return sum(b > a for a, b in pairwise(sea_floor_depths))
-    return sonar_sweep((a+b+c for a, b, c in triplewise(sea_floor_depths)), part=1)
+    return sonar_sweep((a + b + c for a, b, c in triplewise(sea_floor_depths)), part=1)
+
 
 def sea_floor_depths(report: str) -> Iterator[int]:
     return map(int, report.splitlines())
@@ -24,11 +25,10 @@ def main(part: int = 1) -> int:
     with open("2021/data/day01.txt") as f:
         report = f.read()
     return sonar_sweep(sea_floor_depths(report), part)
-    
 
 
-if __name__ == '__main__':
-    report = '''199
+if __name__ == "__main__":
+    report = """199
 200
 208
 210
@@ -37,10 +37,26 @@ if __name__ == '__main__':
 240
 269
 260
-263'''
+263"""
     assert sonar_sweep(sea_floor_depths(report), part=1) == 7
     assert sonar_sweep(sea_floor_depths(report), part=2) == 5
-    assert list(triplewise('ABCDEFG')) == [('A', 'B', 'C'), ('B', 'C', 'D'), ('C', 'D', 'E'), ('D', 'E', 'F'), ('E', 'F', 'G')]
+    assert list(triplewise("ABCDEFG")) == [
+        ("A", "B", "C"),
+        ("B", "C", "D"),
+        ("C", "D", "E"),
+        ("D", "E", "F"),
+        ("E", "F", "G"),
+    ]
+    assert list(triplewise(sea_floor_depths(report))) == [
+        (199, 200, 208),
+        (200, 208, 210),
+        (208, 210, 200),
+        (210, 200, 207),
+        (200, 207, 240),
+        (207, 240, 269),
+        (240, 269, 260),
+        (269, 260, 263),
+    ]
 
     print(main())
     print(main(part=2))
