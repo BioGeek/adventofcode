6c7ec98ea33f378c16b4b18aa34b1e2cb68a6b42
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2021-12-01 17:02:46 +0100

    use black installed with poetry

--- noxfile.py
+++ noxfile.py
@@ -20,5 +20,5 @@ def isort(session):
 @nox.session(python="3.10")
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-isort")
+    session.install("flake8", "flake8-black", "flake8-isort", "flake8-bugbear")
     session.run("flake8", *args)
