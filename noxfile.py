a6f7fb8af40c513927533c0990e93517d13c68d0
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2021-12-04 20:19:17 +0100

    only skip day 2 in mypy check

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 import nox
 
-locations = "2018", "2019", "2020", "2021", "noxfile.py"
+locations = "2017", "2018", "2019", "2020", "2021", "noxfile.py"
 
 
 @nox.session(python="3.10")
@@ -33,5 +35,9 @@ def mypy(session):
         for location in locations:
             # mypy 0.910 doesn't yet support match statements
             # see: https://github.com/python/mypy/pull/10191
-            if location not in ("noxfile.py", "2021"):
+            if location not in ("2021", "noxfile.py"):
                 session.run("mypy", location)
+            else:
+                for day in Path("2021").iterdir():
+                    if day.name not in ("day02.py", "data"):
+                        session.run("mypy", str(day))
