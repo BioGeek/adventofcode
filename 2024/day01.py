bddd670e0b76293af52b73389b45471f4f75efb7
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2024-12-01 20:24:42 +0200

    Lint 2024 day 1

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,23 +1,26 @@
 def total_distance(locations):
     rows = (map(int, loc.split()) for loc in locations.splitlines())
     columns = map(sorted, zip(*rows))
-    diffs = (abs(a-b) for (a, b) in zip(*columns))
+    diffs = (abs(a - b) for (a, b) in zip(*columns))
     return sum(diffs)
+
 
 def similarity_score(locations):
     rows = (map(int, loc.split()) for loc in locations.splitlines())
     left_column, right_column = zip(*rows)
     return sum(number * right_column.count(number) for number in left_column)
-        
+
+
 def main(part: int = 1) -> int:
     with open("2024/data/day01.txt") as f:
         locations = f.read()
-    if part ==1:
+    if part == 1:
         return total_distance(locations)
     else:
         return similarity_score(locations)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     locations = """3   4
 4   3
 2   5
@@ -32,4 +35,3 @@ if __name__ == '__main__':
     assert similarity_score(locations) == 31
 
     print(main(part=2))
-
